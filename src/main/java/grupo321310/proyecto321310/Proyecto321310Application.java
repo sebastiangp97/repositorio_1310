119d07ff1d791ca1e6f5aa1b2d686510b4133ba8
sebastiangp97 <juansgarcia97@gmail.com>
2021-10-13 21:35:59 -0500

    Primer proyecto Spring boot

---
+++ src/main/java/grupo321310/proyecto321310/Proyecto321310Application.java
@@ -0,0 +1,13 @@
+package grupo321310.proyecto321310;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto321310Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto321310Application.class, args);
+	}
+
+}
---
+++ src/test/java/grupo321310/proyecto321310/Proyecto321310ApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo321310.proyecto321310;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto321310ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
